eaa793c59e21ad39aeb09931742d5b5a2d7997f7
Justin Carey <justinthomascarey@gmail.com>
2019-08-22 21:53:33 -0700

    Update champion.py

--- champion.py
+++ champion.py
@@ -1,17 +1,5 @@
-'''
-Copyright (C) BCIT AI/ML Option 2018 Team with Members Following - All Rights Reserved.
-- Jake Jonghun Choi     jchoi179@my.bcit.ca
-- Justin Carey          justinthomascarey@gmail.com
-- Pashan Irani          pashanirani@gmail.com
-- Tony Huang	        tonyhuang1996@hotmail.ca
-- Chil Yuqing Qiu       yuqingqiu93@gmail.com
-Unauthorized copying of this file, via any medium is strictly prohibited.
-Written by Justin Carey <justinthomascarey@gmail.com>
-'''
-
 import ai_state_space_generator, model
 import copy
-
 
 def get_evaluation_score(player, state):
     # Check the side.
